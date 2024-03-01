9a96ecc0e00bd923a87d40d1cfada6674c596db8
Michael <darundaymichael4@gmail.com>
2024-03-01 10:56:01 +0800

    minor modification

--- dashboard.php
+++ dashboard.php
@@ -198,13 +198,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
               <li class="nav-item">
                 <a href="#" class="nav-link active">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Edit Profile</p>
+                  <p>Edit profile</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="#" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p> </p>
+                  <p>Security and login</p>
                 </a>
               </li>
             </ul>
